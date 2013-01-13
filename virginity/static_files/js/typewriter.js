8a129991f731d5e86bf5b6957d0b968b68fb10c1
Steven Melendez <smelendez@wnyc.org>
2013-01-13 16:18:23 -0500

    Lots of tweaks

--- virginity/static_files/js/typewriter.js
+++ virginity/static_files/js/typewriter.js
@@ -1,10 +1,9 @@
-
 var texts = [
 "Not having had intercourse",
 "There is technical virginity, spiritual virginity, and making love",
 "Never having had sexual intercourse or oral sex",
 "Never ejaculating inside a woman’s vagina",
-"The Virgin Mary sense",
+"In the Virgin Mary sense",
 "A personal decision ",
 "A state of mind",
 "Not using a dildo if you are a lesbian",
@@ -17,17 +16,15 @@ var texts = [
 "A state of never having had sex",
 "Innocence from any sort of sexual knowledge",
 "A dying concept that hopefully will one day be obsolete",
-"Never having connected with another person romantically",
+"Never connecting with someone romantically",
 "Having sex with both men and women",
 "Never having had vaginal sex yet",
 "The power that society has over women and their bodies",
-"Someone who has not experienced a consensual sexual encounter, in which one or both persons are nude, when one or both persons experience orgasm",
-"It’s not real; no one is walking around with mine in their pocket",
+"It’s not real! No one is walking around with mine in their pocket",
 "Being pure in body",
 "Never having had penetrative sex",
 "Something we all have in common until one by one we all lose it",
-"The essence of purity, naivety, and lack of intimate experience"
-]
+"The essence of purity, naivety, and lack of intimate experience"];
 $(document).ready(function(){
     function type_text() {
         var textIndex = parseInt(Math.random() * texts.length);
