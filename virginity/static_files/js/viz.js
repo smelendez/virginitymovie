c133c61a9f40f6ed3368a2492279dc0854670b55
Steven Melendez <smelendez@wnyc.org>
2013-01-13 00:12:55 -0500

    Replaced file

---
+++ virginity/static_files/js/viz.js
@@ -0,0 +1,234 @@
+var freeze = false;
+var nopulse = false;
+var svg;
+function nodeclick(d) {
+    open("/story/" + d.pk);
+
+}
+function randInt(x, y) {
+    return Math.floor(Math.random() * (y-x) + x);
+}
+function nodemouseover(d) {
+    freeze = true;
+    if(!nopulse)
+    {
+        svg.selectAll(".node").style("fill","grey");
+        this.style.fill = "red";
+    }
+    d3.select("#popup").style("display", "none");
+    d3.select("#tooltip").text((d.fields.name || "Anon") + ': ' + d.fields.title);
+
+}
+function nodemouseout(d) {
+    freeze = false;
+    if (!nopulse)
+    {
+        this.style.fill = "grey";
+    }
+    d3.select("#tooltip").text("");
+}
+
+function initialize (){
+
+    var nodemap = {};
+    var width = 600;
+    var height = 500;
+
+    var force = d3.layout.force()
+        .charge(-40)
+        .linkDistance(30)
+        .size([width - 50,height]);
+
+    svg = d3.select("#circles").append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+
+
+    d3.json("/api/allstories", function (error, entries) {
+        var links = [];
+        force.nodes(entries)
+        .links(links)
+        .start();
+
+        force.nodes().forEach(function(n) {
+            nodemap[n.pk] = n;
+
+        });
+
+        var node = svg.selectAll("circle.node")
+        .data(entries)
+        .enter().append("circle")
+        .attr("class", "node")
+        .attr("r", function(d) {var r= randInt(5, 10); if (r > 7) {d.up = true;} else d.up = false; return r;})
+        .style("fill", "grey")
+        .on("click", nodeclick, true)
+        .on("mouseover", nodemouseover)
+        .on("mouseout", nodemouseout)
+        .attr("id", function(d) {return d.pk;})
+        .call(force.drag);
+
+        var link = svg.selectAll("line.links")
+        .data(links)
+        .enter().append("line")
+        .attr("class", "link");
+
+        var whichtick = 0;
+        force.on("tick", function() {
+            if(freeze) return;
+            link.attr("x1", function(d) { return d.source.x; })
+                .attr("y1", function(d) { return d.source.y; })
+                .attr("x2", function(d) { return d.target.x; })
+                .attr("y2", function(d) { return d.target.y; });
+
+            
+             node.attr("cx", function(d) { return d.x;})
+                .attr("cy", function(d) { return d.y;});
+
+                
+
+        }); // tick
+
+        var timertick = 0;
+        setInterval(function(){
+            if(freeze) 
+            {
+                timertick = 0;
+                d3.select("popup")
+                .style("display", "none");
+                return;
+            }
+            if (nopulse) {
+                timertick = 0;
+                node.attr("r", 7.5);
+                return;
+
+            }
+            var amount = 0.1;
+            timertick +=1;
+
+            if (timertick % 15 == 0)
+            {
+                // Show popup
+                var allnodes = entries;
+                var numnodes = allnodes.length;
+                var randnode = allnodes[randInt(0, numnodes)];
+                var cx = randnode.x;
+                var cy = randnode.y;
+                var r = 5;
+                node.style("fill", "grey");
+
+
+                d3.select("#popup")
+                    .style("display", "block")
+                    .style("top", cy + r + "px")
+                    .style("left", cx + r + "px")
+                    .text(randnode.fields.name || "Anon");
+
+                document.getElementById(randnode.pk).style.fill="red";
+
+
+            }
+            
+            node.attr("r", function(d) {
+                var r = parseFloat(this.getAttribute("r"));
+                if (d.up)
+                {
+                    r += amount;
+
+                    if (r > 10)
+                    {
+                        r -= amount;
+                        d.up = false;
+                    }
+
+                }
+                else
+                {
+                    r -= amount;
+
+                    if (r < 5)
+                    {
+                        r += amount;
+                        d.up = true;
+                    }
+                }
+                return r;
+                
+
+
+            });
+
+        }, 100);
+
+
+        function no_tag() {
+            link = link.data([]);
+            link.exit().remove();
+            node.style("fill", "grey");
+            force.start();
+
+        }
+        function select_tag(tag, category){
+            var tagnodes = [];
+            links = [];
+            force.links(links);
+            d3.json("/api/tag/" + tag + "/" + category + "/stories", function (error, entries) {
+                node.style("fill","grey");
+                entries.forEach(function(story) {
+                    var storynode = nodemap[story.pk];
+                    document.getElementById(story.pk).style.fill="red";
+
+                    var ntagnodes = tagnodes.length;
+                    tagnodes.push(storynode);
+
+                    if (ntagnodes > 0)
+                    {
+                        links.push({source: storynode, target: tagnodes[Math.floor(Math.random() * ntagnodes)]});
+                    }
+                    
+
+                });
+
+                link.data([]);
+                link = link.data(links);
+
+                link.enter().insert("line", ".node")
+                    .attr("class", "link");
+                link.exit().remove();
+
+                force.start();
+                
+
+            });
+        }
+
+
+        d3.select("aside").on("click", function() {
+            d3.selectAll("a").attr("class","unselected");
+            no_tag();
+            nopulse = false;
+            
+
+        }, true);
+        d3.selectAll("a").on("click", function() {
+            
+            d3.select("#popup").style("display", "none");
+            select_tag(this.text.toLowerCase(), "phrases");
+            nopulse =  true;
+            d3.selectAll("a").attr("class","unselected");
+            this.setAttribute("class", "selected");
+            return false;
+
+        }, true);
+
+
+    });
+}
+
+initialize();
+        
+
+
+        
+
